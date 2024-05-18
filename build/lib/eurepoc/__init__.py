076b1dcf97ca831a194095e3db4ecfa89b5faaf1
camilleswp <camille.borrett@swp-berlin.org>
2024-05-18 14:46:32 +0200

    Remove build, dist, and *.egg-info directories and update .gitignore

--- build/lib/eurepoc/__init__.py
+++
@@ -1,7 +0,0 @@
-# eurepoc/__init__.py
-
-from eurepoc.read_token import read_token
-from eurepoc.database_query import DatabaseQuery
-from eurepoc.incident_dataframes import IncidentDataFrames
-
-__all__ = ['read_token', 'DatabaseQuery', 'IncidentDataFrames']
